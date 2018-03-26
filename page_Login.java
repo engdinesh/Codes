a71ab09904418d77b776d277732fe91195367da3
Dinesh Singh <dinesh.singh270@gmail.com>
2018-03-26 18:43:58 +0530

    Add files via upload

---
+++ page_Login.java
@@ -0,0 +1,13 @@
+package com.test_drawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class page_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page__login);
+    }
+}
